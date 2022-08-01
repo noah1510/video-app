2aceccb4763fff7725924cf15ec894e2156c74ca
noasakurajin <dev@noasakurajin.de>
2022-08-01 13:52:37 +0200

    using imgui to create the window

--- src/main.cpp
+++ src/main.cpp
@@ -6,56 +6,15 @@
 #include "shader.hpp"
 
 using namespace std::literals;
+const std::string glsl_version = "#version 460 core";
+const ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+uint64_t fboWidth = 1920;
+uint64_t fboHeight = 1080;
 
 int main(int argc, const char** argv) {
-    SDL_Window* window;
-
-    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER | SDL_INIT_GAMECONTROLLER) != 0) {
-        printf("Couldn't init GLFW\n");
-        return 1;
-    }
-    
-    // GL 4.6 + GLSL 460
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, 0);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 6);
-    
-    SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "1");
-    SDL_SetHint(SDL_HINT_RENDER_DRIVER, "opengl");
-    SDL_SetHint(SDL_HINT_RENDER_OPENGL_SHADERS, "1");
-    
-    //set all of the opengl ontext flags
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_FORWARD_COMPATIBLE_FLAG);
-    SDL_GL_SetAttribute(SDL_GL_ACCELERATED_VISUAL, 1);
-
-    // Create window with graphics context
-    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
-    SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
-
-    SDL_WindowFlags window_flags = (SDL_WindowFlags)(SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI | SDL_WINDOW_MAXIMIZED);
-    window = SDL_CreateWindow("libtrainsim window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, window_flags);
-    if (!window) {
-        printf("Couldn't open window\n");
-        return 1;
-    }
-
-    VideoReaderState vr_state;
-    if (!video_reader_open(&vr_state, "data/example_video.mp4")) {
-        printf("Couldn't open video file (make sure you set a video file that exists)\n");
-        return 1;
-    }
-
-    auto gl_context = SDL_GL_CreateContext(window);
-    SDL_GL_MakeCurrent(window, gl_context);
-    SDL_GL_SetSwapInterval(1);
-    
-    if (!gladLoadGLLoader((GLADloadproc) SDL_GL_GetProcAddress)) {
-        std::cerr << "Could not create OpenGL context" << std::endl;
-        return -1;
-    }
+    sakurajin::imguiHandler::init();
+    unsigned int FBO = 0, outTexture = 0;
+    sakurajin::imguiHandler::initFramebuffer(FBO,outTexture, fboWidth, fboHeight);
     
     std::shared_ptr<sakurajin::Shader> outputShader;
     try{
@@ -77,6 +36,13 @@ int main(int argc, const char** argv) {
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    
+    //init the video renderer
+    VideoReaderState vr_state;
+    if (!video_reader_open(&vr_state, "data/example_video.mp4")) {
+        printf("Couldn't open video file (make sure you set a video file that exists)\n");
+        return 1;
+    }
 
     // Allocate frame buffer
     constexpr int ALIGNMENT = 128;
@@ -92,10 +58,10 @@ int main(int argc, const char** argv) {
     unsigned int VAO = 0, EBO = 0, VBO = 0;
     float vertices[] = {
         // positions    // texture coords
-         16.0f,  9.0f,  1.0f, 0.0f, // top right
-         16.0f, -9.0f,  1.0f, 1.0f, // bottom right
-        -16.0f, -9.0f,  0.0f, 1.0f, // bottom left
-        -16.0f,  9.0f,  0.0f, 0.0f  // top left 
+         16.0f,  9.0f,  1.0f, 1.0f, // top right
+         16.0f, -9.0f,  1.0f, 0.0f, // bottom right
+        -16.0f, -9.0f,  0.0f, 0.0f, // bottom left
+        -16.0f,  9.0f,  0.0f, 1.0f  // top left 
     };
     unsigned int indices[] = {  
         0, 1, 3, // first triangle
@@ -124,61 +90,104 @@ int main(int argc, const char** argv) {
     glDisable(GL_CULL_FACE);
     
     SDL_Event event;
-    while (event.type != SDL_QUIT && !(event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_ESCAPE)) {
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        // update the viewport
-        int window_width, window_height;
-        SDL_GetWindowSize(window, &window_width, &window_height);
-        //glfwGetFramebufferSize(window, &window_width, &window_height);
-        glViewport(0, 0, window_width, window_height);
+    
+    bool exit = false;
+    while (!exit) {
+        sakurajin::imguiHandler::startRender();
         
-        //create and update the projection matrix
-        float camMult = 0.9;
-        auto orth = glm::ortho(
-            -16.0f, 
-            camMult * 9.0f * window_width / window_height,
-            -9.0f,
-            camMult * 16.0f * window_height / window_width,
-            -10.0f,
-            10.0f
-        );
-        outputShader->setUniform("transform", orth);
+        ImGui::Begin("video out");
+            
+            //update the viewport and load the fbo
+            fboHeight = ImGui::GetWindowHeight();
+            fboWidth = ImGui::GetWindowWidth();
+            
+            auto size = ImGui::GetContentRegionAvail();
+            auto max = ImGui::GetWindowContentRegionMax();
+            auto min = ImGui::GetWindowContentRegionMin();
+            size.x = max.x-min.x;
+            size.y = max.y-min.y;
+            fboWidth = size.x;
+            fboHeight = size.y;
+            
+            sakurajin::imguiHandler::loadFramebuffer(FBO,fboWidth,fboHeight);
+            
+            outputShader->use();
+            //create and update the projection matrix
+            float camMult = 0.8;
+            auto orth = glm::ortho(
+                -16.0f, 
+                camMult * 9.0f * fboWidth / fboHeight,
+                -9.0f,
+                camMult * 16.0f * fboHeight / fboWidth,
+                -10.0f,
+                10.0f
+            );
+            outputShader->setUniform("transform", orth);
 
-        // Read a new frame and load it into texture
-        int64_t pts;
-        if (!video_reader_read_frame(&vr_state, frame_data, &pts)) {
-            printf("Couldn't load video frame\n");
-            return 1;
+            // Read a new frame and load it into texture
+            int64_t pts;
+            if (!video_reader_read_frame(&vr_state, frame_data, &pts)) {
+                printf("Couldn't load video frame\n");
+                return 1;
+            }
+
+            //activate the texture and the shader
+            glActiveTexture(GL_TEXTURE0);
+            glBindTexture(GL_TEXTURE_2D, texRGB);
+            glTexImage2D(
+                GL_TEXTURE_2D, 
+                0, 
+                GL_RGBA, 
+                vr_state.width, 
+                vr_state.height, 
+                0, 
+                GL_RGBA, 
+                GL_UNSIGNED_BYTE, 
+                frame_data
+            );
+            
+            //draw the rectangle
+            glBindVertexArray(VAO);
+            glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+            
+            glBindVertexArray(0);
+            glBindFramebuffer(GL_FRAMEBUFFER, 0);
+            
+            ImGui::Image((void*)(intptr_t)outTexture, size);
+            
+            //show a tooltip and window borders when the window is hovered
+            if(ImGui::IsItemHovered()){
+                ImGui::BeginTooltip();
+                ImGui::Text("pointer = %u", outTexture);
+                ImGui::Text("size = %lu x %lu", fboWidth, fboHeight);
+                ImGui::EndTooltip();
+                
+                ImVec2 vMin = ImGui::GetWindowContentRegionMin();
+                ImVec2 vMax = ImGui::GetWindowContentRegionMax();
+
+                vMin.x += ImGui::GetWindowPos().x;
+                vMin.y += ImGui::GetWindowPos().y;
+                vMax.x += ImGui::GetWindowPos().x;
+                vMax.y += ImGui::GetWindowPos().y;
+
+                ImGui::GetForegroundDrawList()->AddRect( vMin, vMax, IM_COL32( 255, 255, 0, 255 ) );
+            }
+        
+        ImGui::End();
+
+        sakurajin::imguiHandler::endRender();
+        
+        while(SDL_PollEvent(&event)){
+            ImGui_ImplSDL2_ProcessEvent(&event);
+            
+            if(event.type == SDL_QUIT){
+                exit = true;
+                break;
+            } else if(event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_ESCAPE){
+                exit = true;
+                break;
+            }
         }
-
-        //activate the texture and the shader
-        glActiveTexture(GL_TEXTURE0);
-        glBindTexture(GL_TEXTURE_2D, texRGB);
-        glTexImage2D(
-            GL_TEXTURE_2D, 
-            0, 
-            GL_RGBA, 
-            vr_state.width, 
-            vr_state.height, 
-            0, 
-            GL_RGBA, 
-            GL_UNSIGNED_BYTE, 
-            frame_data
-        );
-        
-        outputShader->use();
-        
-        //draw the rectangle
-        glBindVertexArray(VAO);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-        
-        glBindVertexArray(0);
-
-        SDL_GL_SwapWindow(window);
-        std::this_thread::sleep_for(std::chrono::milliseconds(10));
-        SDL_PollEvent(&event);
-        
         /*
         double pt_in_seconds = pts * (double)vr_state.time_base.num / (double)vr_state.time_base.den;
         while (pt_in_seconds > glfwGetTime()) {
